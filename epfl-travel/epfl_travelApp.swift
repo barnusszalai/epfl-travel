161da5255c6cf8a5872330f562885cb49d1fc0b7
Barnabas Szalai <barnusszalai@gmail.com>
2024-09-19 01:32:38 +0200

    Initial Commit

---
+++ epfl-travel/epfl_travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  epfl_travelApp.swift
+//  epfl-travel
+//
+//  Created by Barni Szalai on 19/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct epfl_travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
